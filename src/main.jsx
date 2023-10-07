fbd867ae9d07f8414b5995431bd3d823d3062cf9
Benjamin Sepulveda <benjaminsepulveda.0420@gmail.com>
2023-10-07 05:07:07 -0300

    saad

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <Provider store={store}>
       <GoogleOAuthProvider clientId="469457803946-1surfoqlkb38l947qodpfsv8dp3gsh15.apps.googleusercontent.com">
+        <ChatBot />
         <App />
-         <ChatBot />
       </GoogleOAuthProvider>
     </Provider>
   </>
