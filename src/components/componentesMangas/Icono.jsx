64da7f3cf6f108de90285d85a71ed8e233418353
Benjamin Sepulveda <benjaminsepulveda.0420@gmail.com>
2023-10-07 05:15:20 -0300

    sadxd

--- src/components/componentesMangas/Icono.jsx
+++ src/components/componentesMangas/Icono.jsx
@@ -1,8 +1,10 @@
+import icon from "public/img/icon.webp"
+
 const ChatIcon = ({ onClick, isVisible }) => {
     return (
 
         <div className={`chat-icon z-50 ${isVisible ? 'hidden' : ''}`} onClick={onClick}>
-            <img src="public/img/icon.webp" alt="" />
+            <img src={icon} alt="" />
         </div>
     );
 };
